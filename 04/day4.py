74c8b89719e2455a9f20b3dbbdfb37de6d949d65
Michal Kret <michal.b.kret@jpmchase.com>
2018-12-06 17:34:27 +0000

    Day4, python, ugly progress

--- 04/day4.py
+++ 04/day4.py
@@ -1,8 +1,48 @@
 import re
+import numpy as np
 
 input = open("test_random").readlines()
 
 def find_guard(line):
     guard = re.search(r"#(\d+)", line)
-    return int(guard.group(1)) if guard else False
+    return guard.group(1) if guard else False
 
+def get_goodnight_time(line, action):
+    parsed_line = re.search(r":(\d+)\] (.*)", line)
+    return int(parsed_line.group(1)) if (parsed_line and parsed_line.group(2) == action) else False
+
+def get_sleep(data, idx):
+    down = get_goodnight_time(data[idx], "falls asleep")
+    up = get_goodnight_time(data[idx+1], "wakes up")
+    return idx+2, down, up
+
+def add_sleep(schedule, guard, sleep_time, wake_time):
+    timetable = np.zeros(60)
+    timetable[sleep_time:wake_time] = 1
+    #print("Timetable", guard, "guard\n", schedule)
+    if guard in schedule:
+        schedule[guard] += timetable
+        return schedule[guard]
+    else:
+        schedule[guard] = timetable
+        return schedule[guard]
+
+def main(input_file):
+    data = open(input_file).readlines()
+    data.sort()
+    schedule = {}
+
+    i = 0
+    while i < len(data)-2:
+        guard = find_guard(data[i])
+        print("Guard:", guard, "in", i)
+        if not find_guard(data[i+1]):
+            i, sleep_time, wake_time = get_sleep(data, i+1)
+            schedule = add_sleep(schedule, guard, sleep_time, wake_time)
+        else:
+            i += 1
+
+    print(schedule)
+
+if __name__ == ("__main__"):
+    main("input_short")
--- 04/test_day4.py
+++ 04/test_day4.py
@@ -8,11 +8,27 @@ def test_sort():
     input.sort()
     assert input == reference
 
-def test_parse_guards():
+def test_parse_guards_return_id_as_string():
     result = day4.find_guard("[1518-11-01 00:00] Guard #10 begins shift")
-    assert 10 == result
+    assert '10' == result
 
 def test_no_guard_found_return_false():
     result = day4.find_guard("[1518-11-01 00:05] falls asleep")
     assert False == result
 
+def test_parse_sleep_return_time():
+    result = day4.get_goodnight_time("[1518-11-01 00:05] falls asleep", "falls asleep")
+    assert 5 == result
+
+def test_parse_invalid_value_return_fale():
+    result = day4.get_goodnight_time("asleep", "falls asleep")
+    assert False == result
+
+def test_parse_wakeup_return_time():
+    result = day4.get_goodnight_time("[1518-11-01 00:55] wakes up", "wakes up")
+    assert 55 == result
+
+# def test_find_guard():
+#     arr = np.array([np.zeros(2),np.zeros(2)])
+#     arr[1,1] = 1
+#     assert 1 == int(np.where(arr[1,:] == 1)))
