6828c3ee072eade510a87ae4a96f5fecea519492
Michal Kret <michal.b.kret@jpmchase.com>
2018-12-07 17:14:03 +0000

    Day 5, python: solved part 1

--- 05/day5.py
+++ 05/day5.py
@@ -4,8 +4,16 @@ def trigger_polymer(polymer):
         while i < len(polymer) - 2:
             if polymer[i].swapcase() == polymer[i+1]:
                 polymer = polymer[:i] + polymer[i+2:]
-            i += 1
-        if polymer[0].swapcase() == polymer[1]:
-            return ""
+                if i > 0: i = i - 1
+            else:
+                i += 1
+        if len(polymer) > 1 and polymer[-1].swapcase() == polymer[-2]:
+            return polymer[:-2]
         return polymer
     return ""
+
+def main(inputFile):
+    print("Part 1:", len(trigger_polymer(open(inputFile).readline().strip())))
+
+if __name__ == "__main__":
+    main("input")
--- 05/test_day5.py
+++ 05/test_day5.py
@@ -9,11 +9,24 @@ def test_givenDifferentlyPolarizedPair_destroyThemAndReturnEmpty():
 def test_differentLetters_returnNotDetroyed():
     assert "ab" == trigger_polymer("ab")
 
+def test_destroyablePairAtTheEnd_returnFirstLetter():
+    assert "b" == trigger_polymer("bAa")
+
+def test_destroyablePairAtTheBegining_returnLastLetter():
+    assert "b" == trigger_polymer("aAb")
+
 def test_twoDestroyableNestedPairs_returnEmpty():
     assert "" == trigger_polymer("abBA")
+
+def test_destroyablePairAtTheEnd_returnFirstTwoLetters():
+    assert "ab" == trigger_polymer("abaA")
 
 def test_twoPairsSameCase_returnOriginal():
     assert "aabAAB" == trigger_polymer("aabAAB")
 
 def test_dataFormPage_resultFromPage_lol():
     assert "dabCBAcaDA" == trigger_polymer("dabAcCaCBAcCcaDA")
+    assert 10 == len(trigger_polymer("dabAcCaCBAcCcaDA"))
+
+def test_test():
+    assert "dabCBAcaDabCBAcaDA" == trigger_polymer("dabAcCaCBAcCcaDAaabAcCaCBAcCcaDA")
