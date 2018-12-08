ca23b3dbfc11a289b201f131d66b3e9293b6798d
krtex <krtexxx@gmail.com>
2018-12-08 18:12:51 +0000

    Day 8, python: part 2 solved

--- 08/day8.py
+++ 08/day8.py
@@ -1,24 +1,51 @@
 from functools import reduce
 import re
 
+HEADER = 2
+
+def sum_scope(input):
+    return reduce((lambda x,y: x+y), input)
+
 def sum_metadata(input):
-    HEADER = 2
     children, data = input[:HEADER]
 
-    ch_sum = 0
     ch_length = 0
+    ch_sum = 0
     for i in range(children):
         cl, cs = sum_metadata(input[HEADER + ch_length:])
-        ch_sum += cs
         ch_length += cl
+        ch_sum += cs
 
     my_length = HEADER + ch_length + data
-    my_sum = reduce((lambda x,y: x+y), input[HEADER + ch_length : HEADER + ch_length + data]) + ch_sum
+    my_sum = sum_scope(input[HEADER + ch_length : HEADER + ch_length + data]) + ch_sum
     return my_length, my_sum
+
+def get_root_value(input):
+    children, data = input[:HEADER]
+    my_value = 0
+
+    ch_length = 0
+    ch_values = []
+    for i in range(children):
+        cl, cv = get_root_value(input[HEADER + ch_length:])
+        ch_length += cl
+        ch_values.append(cv)
+
+    if len(ch_values):
+        for v in input[HEADER + ch_length : HEADER + ch_length + data]:
+            if v-1 <  len(ch_values):
+                my_value += ch_values[v-1]
+    else:
+        my_value = sum_scope(input[HEADER:HEADER + data])
+
+    my_length = HEADER + ch_length + data
+    return my_length, my_value
+
 
 def main(inputFile):
     input = list(map(int, open(inputFile).readline().strip().split(' ')))
-    print(sum_metadata(input))
+    print("Part 1", sum_metadata(input))
+    print("Part 2", get_root_value(input))
 
 if __name__ == "__main__":
     main("input")
--- 08/test_day8.py
+++ 08/test_day8.py
@@ -12,3 +12,13 @@ def test_2_children():
     _, sum = sum_metadata([2, 3, 0, 1, 1, 0, 1, 1, 1, 2, 3])
     assert 8 == sum
 
+def test_root_node_no_children():
+    _, root = get_root_value([0, 3, 1, 2, 3])
+    assert 6 == root
+
+def test_root_node_one_child():
+    # child is worth 1
+    # root seeks for value of 1st, 2nd and 3rd child
+    # only 1st is returned
+    _, root = get_root_value([1, 3, 0, 1, 1, 1, 2, 3])
+    assert 1 == root
