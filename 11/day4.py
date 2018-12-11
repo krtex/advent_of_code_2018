611ee2421db71447da59df04ae28bc130a5f4280
Michal Kret <michal.b.kret@jpmchase.com>
2018-12-11 16:58:31 +0000

    Day 11, python: part 2 solved

--- 11/day4.py
+++ 11/day4.py
@@ -1,23 +1,25 @@
 import numpy as np
-import scipy.misc
+
+def create_grid(size):
+    return np.zeros((size + 1, size + 1))
 
 def calculate_power(grid, sn):
-    for x in range(1,len(grid) + 1):
-        for y in range(1,len(grid) + 1):
+    for x in range(1,len(grid)):
+        for y in range(1,len(grid)):
             cell_rack_id = x +10
-            grid[y-1, x-1] = int(((cell_rack_id * y + sn) * cell_rack_id) / 100) % 10 - 5
+            grid[y, x] = int(((cell_rack_id * y + sn) * cell_rack_id) / 100) % 10 - 5
 
 def sum_array(arr):
     return sum(map(sum,arr))
 
 def sum_power_of_9(grid, x, y):
-    return sum_array(grid[y - 1 : y + 3 - 1, x - 1 : x + 3 - 1])
+    return sum_array(grid[y : y + 3, x : x + 3])
 
-def get_position_of_max_power(grid):
+def get_position_of_max_power_of_9(grid):
     max_energy = -float('Inf')
     position = (0, 0)
-    for x in range(1,len(grid) + 1 - 2):
-        for y in range(1,len(grid) + 1 -2):
+    for x in range(1, len(grid) - 2):
+        for y in range(1, len(grid) - 2):
             temp = sum_power_of_9(grid, x, y)
             #print("Power =", temp)
             if temp > max_energy:
@@ -26,15 +28,35 @@ def get_position_of_max_power(grid):
     print("Power =", max_energy)
     return position
 
+def sum_area(grid):
+    for x in range(1,len(grid)):
+        for y in range(1,len(grid)):
+            grid[y, x] = grid[y, x] + grid[y - 1, x] + grid[y, x - 1] - grid[y - 1, x - 1]
+
+def get_position_of_max_power(grid):
+    max_energy = -float('Inf')
+    position = (0, 0)
+    size = 0
+
+    for diag in range(1, len(grid)):
+        for x in range(diag, len(grid)):
+            for y in range(diag, len(grid)):
+                temp = grid[y, x] + grid[y - diag, x - diag] - grid[y - diag, x] - grid[y, x - diag]
+                if temp > max_energy:
+                    max_energy = temp
+                    position = (x - diag + 1, y - diag + 1)
+                    size = diag
+    print("Power =", max_energy)
+    return position, size
+
 def main():
     size = 300
-    grid = np.zeros((size,size))
+    grid = create_grid(size)
     serial_number = 2694
     calculate_power(grid, serial_number)
+    print(get_position_of_max_power_of_9(grid))
+    sum_area(grid)
     print(get_position_of_max_power(grid))
-    #print(grid)
-    scipy.misc.imsave("grid.jpg", grid)
-
 
 if __name__ == "__main__":
     main()
