2a9a1f2cb55ce52c09d4d4506e7d46a739b32c64
Michał <krtexxx@gmail.com>
2018-12-13 01:43:02 +0000

    Day12 part 1

--- 12/day12.py
+++ 12/day12.py
@@ -1,21 +1,28 @@
 import re
 
-def create_header_and_body_from_file(inputFile):
-    with open(inputFile) as myFile:
-        header = "".join(re.findall(r"[#\.]", myFile.readline()))
-        body = "".join(re.findall(r"[#\.]", myFile.read()))
-    return header, body
-
 def to_num(char):
     return 1 if (char == '#') else 0
 
 def to_char(num):
     return '#' if (num == 1) else '.'
 
+def print_pots(pots):
+    word = []
+    while pots:
+        word.append(to_char(pots & 1))
+        pots >>= 1
+    print("".join(word))
+
+def create_header_and_body_from_file(inputFile):
+    with open(inputFile) as myFile:
+        header = "".join(re.findall(r"[#\.]", myFile.readline()))
+        body = "".join(re.findall(r"[#\.]", myFile.read()))
+    return header, body
+
 def create_pots_from_header(header):
     pots = 0
     for i, c in enumerate(header):
-        pots |= to_num(c) << len(header) -1 - i
+        pots |= to_num(c) << i
     return pots
 
 def create_masks_from_text(body):
@@ -26,51 +33,60 @@ def create_masks_from_text(body):
             masks.append((temp, to_num(c)))
             temp = 0
         else:
-            temp |= to_num(c) << 4 - (i % 6)
+            temp |= to_num(c) << (i % 6)
     return masks
 
-def pull_out_plants(pots, length_difference):
+def pull_out_plants(pots, head):
     plants = 0
-    for i in range(pots.bit_length() - 1 -2, length_difference - 1 -4, -1):
-        if pots & 1:
-            plants += i
-            print(plants, i)#if pots & 1 else 0
-        pots >>= 1
+    #print("======================")
+    for n in range(pots.bit_length()):
+        if pots >> n & 1:
+            #print(n)
+            plants += n + 5
+            #print(plants, i)#if pots & 1 else 0
+        #pots >>= 1
     return plants
 
-def print_pots(pots):
-    word = []
-    while pots:
-        word.append(to_char(pots & 1))
-        pots >>= 1
-    print("".join(word[::-1]))
-
-def get_next_generation_based_on_mask(pots, m):
+def get_next_generation_based_on_mask(pots, m, head):
     new_pots = 0
     for n in range(pots.bit_length()):
         if ((m[0] ^ (pots >> n)) & 31) == 0:
             new_pots |= m[1] << n
-    return new_pots
+    return new_pots, 0
+    # return new_pots
 
-def predict_future_of_plants_better_this_time(pots, masks, generation):
+def func(pots):
+    for i in range(pots.bit_length()):
+        if pots >> i & 1:
+            return i
+
+def predict_future_of_plants(pots, masks, generation):
     new_pots = 0
+    head = 0
     for i in range(1, generation + 1):
         for m in masks:
-            pots = pots << 4 if (pots & 15 != 0) else pots
-            new_pots |= get_next_generation_based_on_mask(pots, m)
-        print_pots(new_pots)
-        if i == generation: total_plants = pull_out_plants(new_pots, pots.bit_length() - new_pots.bit_length())
+            while pots & 15 != 0:
+                pots <<= 1
+                head += 1
+            res = get_next_generation_based_on_mask(pots, m, head)
+            new_pots |= res[0]
+            head += res[1]
+            #new_pots |= res
+        head -= func(new_pots)
+        #print("HEAD", head)
+        #print_pots(new_pots)
+        if i == generation: total_plants = pull_out_plants(new_pots, head)
         pots = new_pots
         new_pots = 0
-    print("Total plants in {} generations".format(generation), total_plants)
+    print("Total plants in {} generations is:".format(generation), total_plants, head)
     return new_pots
 
 def main():
-    header, body = create_header_and_body_from_file("input_test")
+    header, body = create_header_and_body_from_file("input")
     pots = create_pots_from_header(header)
     masks = create_masks_from_text(body)
-    generation = 20
-    new_pots = predict_future_of_plants_better_this_time(pots, masks, generation)
+    generation = 50000000000
+    new_pots = predict_future_of_plants(pots, masks, generation)
 
 if __name__ == "__main__":
     main()
